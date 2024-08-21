6e091b950ffc5eea7846c9fb454c0fda693ae756
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-08-21 17:46:39 -0400

    Starting code to render paintings

---
+++ Triple-A/context/ArtProvider.jsx
@@ -0,0 +1,35 @@
+import { handleFetch } from '../utils'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import ArtContext from './ArtContext'
+
+const url = `https://api.artic.edu/api/v1/artworks`
+
+const ArtProvider = ({ children }) => {
+  const [artData, setArtData] = useState([])
+  const [wrong, setError] = useState()
+
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const [data, error] = await handleFetch(url);
+      if (data) setArtData(data);
+      if (error) setError(error);
+    };
+    doFetch()
+  }, [])
+
+  const value = {
+    artData
+  }
+
+  return (
+    <ArtContext.Provider value={value}>
+      {children}
+    </ArtContext.Provider>
+  )
+
+}
+
+
+export default ArtProvider
--- Triple-A/src/App.jsx
+++ Triple-A/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ArtProvider from '../context/ArtProvider'
 
 function App() {
 
