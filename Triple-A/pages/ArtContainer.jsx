080a2c4894c37dcb0cb6e8501632222253e62fb1
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-08-22 16:59:21 -0400

    Fixed issue with image

--- Triple-A/pages/ArtContainer.jsx
+++ Triple-A/pages/ArtContainer.jsx
@@ -10,7 +10,7 @@ function ArtContainer() {
         artData?.map((artwork) => (
           <li key={artwork.id}>
             <h1>{artwork.title}</h1>
-            <img src={artwork.thumbnail.lqip} alt="artworkPainting" />
+            <img style={{ width: 300 }} src={artwork['image_id'] ? `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg` : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"} alt="artworkPainting" />
             <button>Click me for details</button>
           </li>
         ))
