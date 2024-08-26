b6124157a9d98afe437c392823e1132b942c6bb8
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-08-26 15:24:55 -0400

    Styling for artworks & forms

--- Triple-A/components/ArtContainer.jsx
+++ Triple-A/components/ArtContainer.jsx
@@ -13,17 +13,19 @@ function ArtContainer() {
   else data = artData
 
   return (
-    <ul>
-      {
-        data?.map((artwork) => (
-          <li key={artwork.id}>
-            <h1>{artwork.title}</h1>
-            <img style={{ width: 300 }} src={artwork['image_id'] ? `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg` : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"} alt="artworkPainting" />
-            <button onClick={() => navigate(`/${artwork.id}`)}>Click me for details</button>
-          </li>
-        ))
-      }
-    </ul>
+    <div className="artworks-container">
+      <ul className="artworks">
+        {
+          data?.map((artwork) => (
+            <li key={artwork.id} className="artLi">
+              <h1>{artwork.title}</h1>
+              <img style={{ width: 300 }} src={artwork['image_id'] ? `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg` : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"} alt="artworkPainting" />
+              <button onClick={() => navigate(`/${artwork.id}`)}>Click me for details</button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
